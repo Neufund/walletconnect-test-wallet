49a8a0b3de371ed61a6c5605ee4e931c2195fa32
Tomek Banach <tomek@banciur.org>
2020-03-20 00:51:21 +0100

    include compiled project

---
+++ build/precache-manifest.b4b7a6d9541a4e579f214e283664369a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6e4e9f5f97caabad8127",
+    "url": "./static/js/main.6e4e9f5f.chunk.js"
+  },
+  {
+    "revision": "d36ad68fa8b0f76ca592",
+    "url": "./static/js/1.d36ad68f.chunk.js"
+  },
+  {
+    "revision": "a39ee2c5e221f15049c01f1f43e26e50",
+    "url": "./index.html"
+  }
+];
